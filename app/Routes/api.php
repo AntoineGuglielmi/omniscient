f6349e7d33f087516166db8f2c82392658331dcf
Antoine Guglielmi <antoine.guglielmi@insign.fr>
2022-03-21 14:46:43 +0100

    add api route budgets-home

--- app/Routes/api.php
+++ app/Routes/api.php
@@ -3,3 +3,7 @@
 $api->get('/([a-z]+)')
     ->callback(['HelloWorldController::hello_world_action'])
     /* ->name('hello_world') */;
+
+$api->get('/budgets-home')
+    ->callback(['BudgetsController::get_home'])
+    /* ->name('hello_world') */;
